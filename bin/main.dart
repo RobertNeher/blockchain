b75cf5b9f2982e25b2d73baa9b0661272fb736db
rneher <robert.neher@bartscher-hasenaecker.de>
2021-12-02 16:03:54 +0100

    compare method added. Comparing the hash against given string

--- bin/main.dart
+++ bin/main.dart
@@ -1,23 +1,27 @@
 import 'block.dart';
 import 'blockchain.dart';
+import 'dart:math';
+import 'dart:io';
+import 'package:intl/intl.dart';
 
 void main(List<String> args) {
+  final intFormat = NumberFormat('00000');
+  Map<String, dynamic> data;
+  final Random secureRandom = Random.secure();
   BlockChain bc = BlockChain();
 
-  bc.add('First Block');
-  bc.add('Second Block');
-  bc.add('Third Block');
-  bc.add('Fourth Block');
-  bc.add('Fifth Block');
-  bc.add('Sixth Block');
-  bc.add('Seventh Block');
-  bc.add('Eighth Block');
-  bc.add('Nineth Block');
-  bc.add('Tenth Block');
-  print(bc.length());
-
+  for (int i = 0; i < 100; i++) {
+    data = {
+      'transaction': secureRandom.nextInt(100000),
+      'amount': secureRandom.nextDouble().toString(),
+      'desription': 'Beschreibung: $i'
+    };
+    stdout.write('${intFormat.format(i)}\r');
+    bc.add(data.toString());
+  }
+  DateTime start = DateTime.now();
   for (Block block in bc.getBlocks()) {
     print(
-        '${String.fromCharCodes(block.data)}\t${block.timeStamp}\t${block.hash}\t${block.previousHash}');
+        '${String.fromCharCodes(block.data)}\t${block.timeStamp}\t${block.hash}');
   }
 }
